ad9da120a867ffb7981bfda8a321a8bb7005f8f9
Kim Nay <kchhuok@gmail.com>
2019-10-21 11:42:41 +0200

    update

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,16 +1,29 @@
 
-require 'gossip'
-require 'view'
+require 'bundler'
+Bundler.require
+$:.unshift File.expand_path('./../lib', __FILE__)
+require 'app/controller'
+require 'app/gossip'
+require 'app/router'
+require 'views/view'
+
 
 class Controller
-    attr_accessor :view, :gossip, :params
-    
-    def create_gossip
-        params =Hash.new #On crée un nouvelle hash, en gros je met mes paramètres dans un hash
-        params = @view.create_gossip #
-        @gossip = Gossip.new(“Jean-Michel Concierge”, “Féfé est de Bordeaux”)    #2.2.5
-        @gossip.save #2.2.5
-    end
-end
+	attr_accessor :gossip_instance, :view_instance
 
+	def initialize
+		@view_instance = View.new
+
+	end
+
+	def create_gossip
+		params_hash = @view_instance.create_gossip
+		@gossip_instance = Gossip.new(params_hash)
+		@gossip_instance.save
+	end
+
+	def view_gossip
+		params_array = Gossip.all
+		@view_instance.view_all(params_array)
+	
 
